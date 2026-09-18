ccec507ac27ff738c6ad556159749ca66e54c0c5
ERIK PETERSON <ERIK412CLASS@gmail.com>
2026-09-18 18:52:12 -0500

    update

--- transactions.js
+++ transactions.js
@@ -1,20 +1,35 @@
-const billData = {
-  company: "",
-  begBalance: "",
-  dueDate: "",
-  paymentAmt: "",
-  autoPay: "",
-};
-
 function getBillData() {
-  billData.company = document.getElementById("company").value.toUpperCase();
-  billData.begBalance = document.getElementById("beginning-balance").value;
-  billData.begBalance = parseFloat(billData.begBalance);
-  billData.dueDate = document.getElementById("due-date").value;
-  billData.paymentAmt = document.getElementById("payment-amount").value;
-  billData.autoPay = document.getElementById("auto-pay").checked;
+  let begBalance = parseFloat(document.getElementById("beg-balance").value);
+  if (isNaN(begBalance)) {
+    begBalance = 0;
+  }
+  const dueDate = Number(document.getElementById("due-date").value);
+  const dueDateError = document.getElementById("due-date-error");
+  dueDateError.textContent = "";
+  if (dueDate < 1 || dueDate > 31) {
+    dueDateError.textContent = "Please enter a value between 1 and 31.";
+    return;
+  }
+
+  let paymentAmt = parseFloat(document.getElementById("payment-amt").value);
+  if (isNaN(paymentAmt)) {
+    paymentAmt = 0;
+  }
+
+  const billData = {
+    company: document.getElementById("company").value.toUpperCase(),
+    begBalance: parseFloat(begBalance),
+    dueDate: Number(dueDate),
+    paymentAmt: parseFloat(paymentAmt),
+    autoPay: document.getElementById("auto-pay").checked,
+  };
+
   return billData;
 }
 
-let btnSubmit = document.getElementById("btn-bill-submit");
-btnSubmit.addEventListener("click", getBillData);
+let btnSubmit = document.getElementById("btn-submit");
+
+btnSubmit.addEventListener("click", function () {
+  const data = getBillData();
+  console.log(data);
+});
